0accd2e56942f289177c8374584e82b09d544d21
CharlieOuthwaite <charlotteouthwaite@gmail.com>
2024-05-03 11:29:42 +0100

    Coded up peaks and plots

--- 0_Record_based_values.R
+++ 0_Record_based_values.R
@@ -16,6 +16,9 @@ rm(list = ls())
 library(terra)
 library(sparta)
 library(sf)
+library(dplyr)
+library(splus2R)
+library(ggplot2)
 
 # set directories
 datadir <- "0_Data/"
@@ -30,8 +33,8 @@ irecord_ladybirds <- read.csv(paste0(datadir, "iRecord_Ladybirds/records-2024-03
 summary(names(ladybird_survey) == names(irecord_ladybirds)) # column names the same
 lb_dat <- rbind(ladybird_survey, irecord_ladybirds)
 
-
 names(lb_dat)
+
 # subset to desired columns
 lb_dat <- lb_dat[, c(17, 18, 30, 38, 44, 45, 66, 71:73, 87, 95, 111, 129, 130, 138, 141, 150, 155, 163, 179, 190, 191, 194, 205, 211:217, 220:229)]
 
@@ -54,6 +57,8 @@ table(lb_dat$taxonRank.processed)
 #    146              7110               153            271260               295 
 
 table(lb_dat$stateProvince.processed)
+
+### subsetting the data ###
 
 # Select only those to the species level
 lb_dat <- lb_dat[lb_dat$taxonRank.processed == "species", ] # 271260
@@ -90,7 +95,8 @@ lb_dat <- lb_dat[lb_dat$year.processed >= 1970, ] # 262732
 # Select records for Great Britain only
 lb_dat <- lb_dat[lb_dat$stateProvince.processed %in% c("England", "Scotland", "Wales"), ] # 259281
 
-
+# save the organised dataset
+write.csv(lb_dat, paste0(outdir, "/GB_ladybird_occurrences_processed.csv"))
 
 ############################################################
 #                                                          #
@@ -102,14 +108,52 @@ lb_dat <- lb_dat[lb_dat$stateProvince.processed %in% c("England", "Scotland", "W
 # trait database.
 
 # create a table with a row per species to save required information
-sp_tab <- data.frame(species = sort(unique(lb_dat$scientificName.processed)))
+sp_tab <- data.frame(species = sort(unique(lb_dat$scientificName.processed))) # 53 species
 #sp_tab <- read.csv(paste0(outdir, "Species_record_Summaries.csv"))
 
 #### Convert lat/lons to spatial points to use for data extraction ####
 
-lb_xy <- lb_dat[ , c("decimalLongitude.processed", "decimalLatitude.processed")]
-lb_xy <- vect(lb_xy, geom =c("decimalLongitude.processed", "decimalLatitude.processed"))
+lb_xy <- vect(lb_dat, geom =c("decimalLongitude.processed", "decimalLatitude.processed"))
 #plot(lb_xy) # take a look
+
+
+
+##%######################################################%##
+#                                                          #
+####           Number of records per species            ####
+#                                                          #
+##%######################################################%##
+
+# count number of records per species
+vals <- as.data.frame(table(lb_dat$scientificName.processed))
+
+# change column names
+names(vals) <- c("species", "n_records")
+
+# merge sp_tab with vals
+sp_tab <- merge(sp_tab, vals)
+
+
+
+##%######################################################%##
+#                                                          #
+####             Year of most recent record             ####
+#                                                          #
+##%######################################################%##
+
+
+
+yr_tab <- lb_dat %>% group_by(scientificName.processed)  %>% 
+  summarise(yr_recent = max(year.processed))
+
+# change column names
+names(yr_tab) <- c("species", "Most_recent_yr")
+
+# merge sp_tab with vals
+sp_tab <- merge(sp_tab, yr_tab)
+
+# save the table
+write.csv(sp_tab, paste0(outdir, "Species_record_Summaries.csv"), row.names = F)
 
 
 ############################################################
@@ -122,8 +166,13 @@ lb_xy <- vect(lb_xy, geom =c("decimalLongitude.processed", "decimalLatitude.proc
 # This section determines entries for the following columns: Present_England, 
 # Present_Wales, Present_Scotland. 
 
+sp_tab$nrecs_England <- NA
 sp_tab$Present_England <- NA
+
+sp_tab$nrecs_Wales <- NA
 sp_tab$Present_Wales <- NA
+
+sp_tab$nrecs_Scotland <- NA
 sp_tab$Present_Scotland <- NA
 
 for(i in 1:nrow(sp_tab)){
@@ -132,30 +181,36 @@ for(i in 1:nrow(sp_tab)){
   
   if("England" %in% sp_recs$stateProvince.processed) {
     
+    sp_tab[i, "nrecs_England"] <- nrow(sp_recs[sp_recs$stateProvince.processed == "England", ])
     sp_tab[i, "Present_England"] <- 1
     
   }else{
     
+    sp_tab[i, "nrecs_England"] <- 0
     sp_tab[i, "Present_England"] <- 0
     
   }
   
   if("Wales" %in% sp_recs$stateProvince.processed) {
     
+    sp_tab[i, "nrecs_Wales"] <- nrow(sp_recs[sp_recs$stateProvince.processed == "Wales", ])
     sp_tab[i, "Present_Wales"] <- 1
     
   }else{
     
+    sp_tab[i, "nrecs_Wales"] <- 0
     sp_tab[i, "Present_Wales"] <- 0
     
   }
   
   if("Scotland" %in% sp_recs$stateProvince.processed) {
     
+    sp_tab[i, "nrecs_Scotland"] <- nrow(sp_recs[sp_recs$stateProvince.processed == "Scotland", ])
     sp_tab[i, "Present_Scotland"] <- 1
     
   }else{
     
+    sp_tab[i, "nrecs_Scotland"] <- 0
     sp_tab[i, "Present_Scotland"] <- 0
     
   }
@@ -278,7 +333,69 @@ write.csv(sp_tab, paste0(outdir, "Species_record_Summaries.csv"), row.names = F)
 
 
 # This section uses the records to determine when the first, last and peak of 
-# observations of the larvae (where possible) and the adults are from.
+# observations are. We assume that most records are of adults and so results
+# here will reflect the adult peaks. Larvae peaks have been determined by PB. 
+
+sp_tab$Adult_first <- NA
+sp_tab$Adult_last <- NA
+sp_tab$Adult_peak1 <- NA
+sp_tab$Adult_peak2 <- NA
+
+
+for(i in 1:nrow(sp_tab)){
+  # i <- 1
+  
+  # subset to each species 
+  sp_recs <- lb_dat[lb_dat$scientificName.processed == sp_tab[i, 1], ]
+  
+  # identify month of first record
+  sp_tab[sp_tab$species == sp_tab[i, 1], "Adult_first"] <- min(sp_recs$month.processed)
+  
+  # identify month of last record
+  sp_tab[sp_tab$species == sp_tab[i, 1], "Adult_last"] <- max(sp_recs$month.processed)
+  
+  
+  # determine peaks
+  
+  # frequency table of months
+  months_freq <- sp_recs %>% 
+    group_by(month.processed) %>% tally()
+  
+    # get the first month with the most records
+  sp_tab[sp_tab$species == sp_tab[i, 1], "Adult_peak1"] <- grep(TRUE, peaks(months_freq$n))[1]
+  
+  # get the second month with the most records
+  sp_tab[sp_tab$species == sp_tab[i, 1], "Adult_peak2"] <- grep(TRUE, peaks(months_freq$n))[2]
+  
+}
+
+
+# save the table
+write.csv(sp_tab, paste0(outdir, "Species_record_Summaries.csv"), row.names = F)
+
+
+
+# create histograms for each species to refer to
+
+for(i in 1:nrow(sp_tab)){
+  # i <- 1
+  
+  # subset to each species 
+  sp_recs <- lb_dat[lb_dat$scientificName.processed == sp_tab[i, 1], ]
+  
+  ggplot(sp_recs) + 
+    geom_bar(aes(x = factor(month.processed)), 
+             bins = 12, 
+             col = "darkblue", 
+             fill = "lightblue") + 
+    #scale_x_continuous(limits = c(1, 12)) + 
+    theme_light() + 
+    ggtitle(paste0("Frequency of records by month - ", sp_tab[i, 1]))
+  
+ggsave(paste0(outdir, "Histograms/", sp_tab[i, 1], ".png"))
+
+}
+
 
 
 
